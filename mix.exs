e205f74a40d0b6488b7c1cff75289256b8ec86f0
Susumu Yamazaki <zacky1972@gmail.com>
2021-11-15 09:34:20 +0900

    change version

--- mix.exs
+++ mix.exs
@@ -1,10 +1,12 @@
 defmodule Excv.MixProject do
   use Mix.Project
 
+  @version "0.1.0-dev"
+
   def project do
     [
       app: :excv,
-      version: "0.1.0",
+      version: @version,
       elixir: "~> 1.12",
       start_permanent: Mix.env() == :prod,
       deps: deps()
