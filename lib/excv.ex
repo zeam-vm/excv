6b680ab0c1709b04dddf35357ef76470df327a58
Susumu Yamazaki <zacky1972@gmail.com>
2021-11-16 11:14:12 +0900

    remove hello

--- lib/excv.ex
+++ lib/excv.ex
@@ -2,17 +2,4 @@ defmodule Excv do
   @moduledoc """
   Excv is a bridge between OpenCV and Nx.
   """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Excv.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
