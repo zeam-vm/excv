b322260cdc2aed84cf2b4dbed0998831ef5abb0e
Susumu Yamazaki <zacky1972@gmail.com>
2021-11-21 05:23:46 +0900

    fix #22

--- c_src/excv/error.c
+++ c_src/excv/error.c
@@ -3,10 +3,16 @@
 #include <string.h>
 #include <erl_nif.h>
 
+size_t strnlength(const char *s, size_t n)
+{
+    const char *found = memchr(s, '\0', n);
+    return found ? (size_t)(found - s) : n;
+}
+
 ERL_NIF_TERM make_error(ErlNifEnv *env, const char *message)
 {
     ErlNifBinary reason;
-    if(__builtin_expect(!enif_alloc_binary(strnlen(message, 256), &reason), false)) {
+    if(__builtin_expect(!enif_alloc_binary(strnlength(message, 256), &reason), false)) {
         return enif_make_atom(env, "error");
     } else {
         memcpy(reason.data, message, reason.size);
